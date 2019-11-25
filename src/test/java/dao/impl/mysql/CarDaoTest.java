a356d6ea3077539f1a269f612f44b03e5e5bf516
dmitryRashevsky <dmitryrasheuski@gmail.com>
2019-11-25 16:42:20 +0300

    The package src/test/Java was renamed to src/test/java

--- src/test/java/dao/impl/mysql/CarDaoTest.java
+++ src/test/java/dao/impl/mysql/CarDaoTest.java
@@ -15,7 +15,7 @@ import java.sql.SQLException;;
 import java.util.ArrayList;
 import java.util.List;
 
-public class TestCarDao {
+public class CarDaoTest {
 
     private static DaoFactory daoFactory;
     private static UserDao userDao;
