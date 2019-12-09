5afa784733109758e55eb1c9e8fe9a191f374485
dmitryRashevsky <dmitryrasheuski@gmail.com>
2019-12-09 18:36:29 +0300

    The OrderDaoTest was changed for new interface

--- src/test/java/dao/impl/mysql/OrderDaoTest.java
+++ src/test/java/dao/impl/mysql/OrderDaoTest.java
@@ -1,6 +1,5 @@
 package dao.impl.mysql;
 
-import appException.dao.AppSqlException;
 import dao.interfaces.DaoFactory;
 import dao.interfaces.CarDao;
 import dao.interfaces.OrderDao;
@@ -8,21 +7,21 @@ import dao.interfaces.UserDao;
 import entity.car.Car;
 import entity.car.CarBuilder;
 import entity.order.Order;
+import entity.order.OrderBuilder;
 import entity.user.User;
 import entity.user.UserBuilder;
 import org.junit.*;
-import org.junit.rules.ExpectedException;
 
 import java.sql.SQLException;
+import java.util.List;
 
 public class OrderDaoTest {
 
-    private static DaoFactory factory;
+    private static DaoFactory daoFactory;
     private static OrderDao orderDao;
     private static UserDao userDao;
     private static CarDao carDao;
 
-    private static Order order;
     private static String from = "from";
     private static String where = "where";
     private static String comments = "comments";
@@ -30,84 +29,67 @@ public class OrderDaoTest {
     private static User passenger;
     private static User driver;
     private static Car car;
-
-
+    private Order order;
 
     @BeforeClass
     public static void setUp() throws Exception {
-        factory = DaoFactory.getFactory(DaoFactory.TypesDatabases.MY_SQL);
-        orderDao = factory.getOrderDao();
-        userDao = factory.getUserDao();
-        carDao = factory.getCarDao();
+        daoFactory = DaoFactory.getFactory(DaoFactory.TypesDatabases.MY_SQL);
+        orderDao = daoFactory.getOrderDao();
+        userDao = daoFactory.getUserDao();
+        carDao = daoFactory.getCarDao();
 
         passenger = createTestPassenger();
         driver = createTestDriver();
         car = createTestCar(driver.getId());
-        order = new Order();
     }
     @AfterClass
     public static void tearDown() throws Exception {
-
-        delUser(passenger.getId());
-        delUser(driver.getId());
         //car deleted with driver because in cars table "on delete cascade"
+        userDao.deleteUser(driver.getId());
+        userDao.deleteUser(passenger.getId());
+        daoFactory.closeDatasource();
     }
 
-    @Rule
-    public final ExpectedException expectedException = ExpectedException.none();
+    @Before
+    public void setUpMethod() throws SQLException {
+        order = OrderBuilder.createOrder().setIdUser(passenger.getId()).setIdCar(car.getId()).setFrom(from).setWhere(where).setComments(comments).getOrder();
+        long id = orderDao.addOrder(order).orElseThrow(NullPointerException::new);
+        order.setId(id);
+    }
+    @After
+    public void tearDownMethod() throws SQLException {
+        orderDao.deleteOrder(order.getId()).orElseThrow(NullPointerException::new);
+    }
 
     @Test
-    public void test() throws SQLException, AppSqlException {
+    public void getListOrderByUser() throws SQLException {
+        Order order_2 = OrderBuilder.createOrder().setIdUser(passenger.getId()).setIdCar(car.getId()).setFrom(from).setWhere(where).setComments(comments).getOrder();
+        long id2 = orderDao.addOrder(order_2).orElseThrow(NullPointerException::new);
+        order_2.setId(id2);
 
-        order.setIdUser(passenger.getId());
-        order.setIdCar(car.getId());
-        order.setFrom(from);
-        order.setWhere(where);
-        order.setComments(comments);
+        List<Order> list = orderDao.getListByIdUser(passenger.getId()).orElseThrow(NullPointerException::new);
+        Assert.assertTrue(list.contains(order));
+        Assert.assertTrue(list.contains(order_2));
 
-        long id = orderDao.addOrder(order);
-        order.setId(id);
-
-        Order orderDb = orderDao.getListByIdUser(passenger.getId()).get(0);
-        Assert.assertEquals(orderDb, order);
-
-        orderDao.deleteOrder(id);
-        expectedException.expect(AppSqlException.class);
-        orderDao.getListByIdUser(passenger.getId());
+        orderDao.deleteOrder(id2);
     }
 
-    private static User createTestDriver() throws SQLException, AppSqlException {
-
+    private static User createTestDriver() throws SQLException {
         User d = UserBuilder.createUser().setPhone(101).setName("driver").setSurname("order").setPassword("test").setStatus("driver").getUser();
-        long id = userDao.addUser(d);
+        long id = userDao.addUser(d).orElseThrow(NullPointerException::new);
         d.setId(id);
-
         return d;
     }
-    private static User createTestPassenger() throws SQLException, AppSqlException {
-
+    private static User createTestPassenger() throws SQLException {
         User p = UserBuilder.createUser().setPhone(102).setName("passenger").setSurname("order").setPassword("test").getUser();
-        long id = userDao.addUser(p);
+        long id = userDao.addUser(p).orElseThrow(NullPointerException::new);
         p.setId(id);
-
         return p;
-
     }
-    private static Car createTestCar(long idDriver) throws SQLException, AppSqlException {
-
+    private static Car createTestCar(long idDriver) throws SQLException {
         Car c = CarBuilder.createCar().setNumber("1395КН5").setIdDriver(idDriver).setColor("testColor").setModel("testModel").setStatus(true).getCar();
-        long id = carDao.addCar(c);
+        long id = carDao.addCar(c).orElseThrow(NullPointerException::new);
         c.setId(id);
-
         return c;
-
-    }
-    private static void delUser(long id) throws SQLException, AppSqlException {
-
-       userDao.deleteUser(id);
-
-    }
-    private static void delCar(long id) throws SQLException, AppSqlException {
-        carDao.deleteCar(id);
     }
 }
