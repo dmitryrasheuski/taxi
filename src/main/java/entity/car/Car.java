24924bc99cf17b83df1c8a776bd6fe5b2ccc346e
dmitryRashevsky <dmitryrasheuski@gmail.com>
2019-12-23 18:17:48 +0300

    Classes Car, Order, User was marked @Data and deleted all getter, setter, equals() and hashCode()

--- src/main/java/entity/car/Car.java
+++ src/main/java/entity/car/Car.java
@@ -1,7 +1,8 @@
 package entity.car;
 
-import java.util.Objects;
+import lombok.Data;
 
+@Data
 public class Car {
     private long id;
     private String number;
@@ -9,59 +10,4 @@ public class Car {
     private String color;
     private String model;
     private boolean active;
-
-    public long getId() {
-        return id;
-    }
-    public String getNumber() {
-        return number;
-    }
-    public long getIdDriver() {
-        return idDriver;
-    }
-    public String getColor() {
-        return color;
-    }
-    public String getModel() {
-        return model;
-    }
-    public boolean isActive() {
-        return active;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-    public void setNumber(String number) {
-        this.number = number;
-    }
-    public void setIdDriver(long idDriver) {
-        this.idDriver = idDriver;
-    }
-    public void setColor(String color) {
-        this.color = color;
-    }
-    public void setModel(String model) {
-        this.model = model;
-    }
-    public void setActive(boolean active) {
-        this.active = active;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (!(o instanceof Car)) return false;
-        Car car = (Car) o;
-        return id == car.id &&
-                idDriver == car.idDriver &&
-                active == car.active &&
-                Objects.equals(number, car.number) &&
-                Objects.equals(color, car.color) &&
-                Objects.equals(model, car.model);
-    }
-    @Override
-    public int hashCode() {
-        return Objects.hash(id, number, idDriver, color, model, active);
-    }
 }
--- src/main/java/entity/order/Order.java
+++ src/main/java/entity/order/Order.java
@@ -1,7 +1,8 @@
 package entity.order;
 
-import java.util.Objects;
+import lombok.Data;
 
+@Data
 public class Order {
     private long id;
     private long idUser;
@@ -9,59 +10,4 @@ public class Order {
     private String from;
     private String where;
     private String comments;
-
-    public long getId() {
-        return id;
-    }
-    public long getIdUser() {
-        return idUser;
-    }
-    public long getIdCar() {
-        return idCar;
-    }
-    public String getFrom() {
-        return from;
-    }
-    public String getWhere() {
-        return where;
-    }
-    public String getComments() {
-        return comments;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-    public void setIdUser(long idUser) {
-        this.idUser = idUser;
-    }
-    public void setIdCar(long idCar) {
-        this.idCar = idCar;
-    }
-    public void setFrom(String from) {
-        this.from = from;
-    }
-    public void setWhere(String where) {
-        this.where = where;
-    }
-    public void setComments(String comments) {
-        this.comments = comments;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (!(o instanceof Order)) return false;
-        Order order = (Order) o;
-        return id == order.id &&
-                idUser == order.idUser &&
-                idCar == order.idCar &&
-                Objects.equals(from, order.from) &&
-                Objects.equals(where, order.where) &&
-                Objects.equals(comments, order.comments);
-    }
-    @Override
-    public int hashCode() {
-        return Objects.hash(id, idUser, idCar, from, where, comments);
-    }
 }
