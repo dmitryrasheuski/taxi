5ae7c3deb67dfca9e63e439cb6779dd54743b12c
dmitryRashevsky <dmitryrasheuski@gmail.com>
2019-12-25 10:48:29 +0300

    A new types was added to the project. It is 'Color' and 'CarModel'. They was annotated with @Date

---
+++ src/main/java/entity/car/CarModel.java
@@ -0,0 +1,9 @@
+package entity.car;
+
+import lombok.Data;
+
+@Data
+public class CarModel {
+    private int id;
+    private String title;
+}
---
+++ src/main/java/entity/car/Color.java
@@ -0,0 +1,9 @@
+package entity.car;
+
+import lombok.Data;
+
+@Data
+public class Color {
+    private int id;
+    private String title;
+}
