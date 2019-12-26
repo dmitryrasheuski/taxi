160fe4c02b784c060f45076827d01674453e384e
dmitryRashevsky <dmitryrasheuski@gmail.com>
2019-12-26 11:42:16 +0300

    'throws' section  was added to all method from 'AddressDao' class

--- src/main/java/dao/interfaces/AddressDao.java
+++ src/main/java/dao/interfaces/AddressDao.java
@@ -2,9 +2,10 @@ package dao.interfaces;
 
 import entity.order.Address;
 
+import java.sql.SQLException;
 import java.util.Optional;
 
 public interface AddressDao {
-    Optional<Address> getAddress(long id);
-    Optional<Address> getAddress(String title);
+    Optional<Address> getAddress(long id) throws SQLException;
+    Optional<Address> getAddress(String title) throws SQLException;
 }
