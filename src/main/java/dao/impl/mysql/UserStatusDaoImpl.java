47957821dc28aa6614eff5a70e154046ee75463b
dmitryRashevsky <dmitryrasheuski@gmail.com>
2019-12-26 17:03:36 +0300

    'getUserStatus(String title)' method from 'UserStatusDao' class was replaced by 'mergeUserStatus(UserStatus status)', so implementor 'UserStatusDaoImpl' was changed

--- src/main/java/dao/impl/mysql/UserStatusDaoImpl.java
+++ src/main/java/dao/impl/mysql/UserStatusDaoImpl.java
@@ -1,33 +1,39 @@
 package dao.impl.mysql;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import entity.user.UserStatus;
+import lombok.extern.log4j.Log4j;
+
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
+@Log4j
 public class UserStatusDaoImpl extends AbstractDao implements dao.interfaces.UserStatusDao {
-    private static final String getStatusId = "SELECT id FROM userStatus WHERE status = ?";
+    private static final String addUserStatus = "INSERT INTO user_status(title) VALUE (?)";
+    private static final String getUserStatusId = "SELECT id FROM user_status WHERE title = ?";
 
     UserStatusDaoImpl(MysqlDaoFactory factory){
         super(factory);
     }
 
     @Override
-    public Optional<Integer> getUserStatusId(String title) {
-        try {
-            return getEntityByOneValue(title, getStatusId)
-                    .flatMap(list -> Optional.of((Integer) list.get(0)));
-        } catch (SQLException e) {
-            return Optional.empty();
+    public Optional<Integer> mergeUserStatus(UserStatus status) throws SQLException {
+        Optional<Integer> res = getEntityByOneValue(status.getTitle(), getUserStatusId)
+                .map((list) -> ((List<Integer>)list).get(0));
+        if(!res.isPresent()) {
+            res = addEntity(status, addUserStatus)
+                    .map((id) -> ((Long)id).intValue());
         }
+        return res;
     }
 
     @Override
     PreparedStatement getPreparedStatementForAddEntity(Connection con, PreparedStatement ps, String sqlInsert, Object entity) throws SQLException {
-        return null;
+        UserStatus status = (UserStatus) entity;
+        ps = con.prepareStatement(sqlInsert, Statement.RETURN_GENERATED_KEYS);
+        ps.setString(1, status.getTitle());
+        return ps;
     }
     @Override
     Optional<List<Integer>> handleResultSet(ResultSet rs) throws SQLException {
--- src/main/java/dao/interfaces/UserStatusDao.java
+++ src/main/java/dao/interfaces/UserStatusDao.java
@@ -2,8 +2,9 @@ package dao.interfaces;
 
 import entity.user.UserStatus;
 
+import java.sql.SQLException;
 import java.util.Optional;
 
 public interface UserStatusDao {
-    Optional<Integer> getUserStatusId(String title);
+    Optional<Integer> mergeUserStatus(UserStatus status) throws SQLException;
 }
