c64d68a85b9512b2bc4d85137930ece29a9ed948
dmitryRashevsky <dmitryrasheuski@gmail.com>
2019-12-30 15:22:34 +0300

    The 'CarModel' class was annotated with the annotation '@RequiredArgsConstructor'

--- src/main/java/entity/car/CarModel.java
+++ src/main/java/entity/car/CarModel.java
@@ -3,9 +3,11 @@ package entity.car;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@RequiredArgsConstructor
 public class CarModel {
     private int id;
     @NonNull
