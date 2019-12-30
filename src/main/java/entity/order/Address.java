326f62b9106dbf45ad818a813576df8a490d4072
dmitryRashevsky <dmitryrasheuski@gmail.com>
2019-12-30 18:55:39 +0300

    The 'Address' class was annotated '@AllArgsConstructor' and '@RequiredArgsConstructor'

--- src/main/java/entity/order/Address.java
+++ src/main/java/entity/order/Address.java
@@ -1,9 +1,13 @@
 package entity.order;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
 
 @Data
+@AllArgsConstructor
+@RequiredArgsConstructor
 public class Address {
     private long id;
     @NonNull
