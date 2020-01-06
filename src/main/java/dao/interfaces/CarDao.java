2b5f0a50c3a39b90e41249a06e31d65bd7c563e7
dmitryRashevsky <dmitryrasheuski@gmail.com>
2020-01-06 20:15:12 +0300

    The 'updateStatus()' and 'getListCarsByStatus()' methods was removed from the 'CarDao' interface

--- src/main/java/dao/interfaces/CarDao.java
+++ src/main/java/dao/interfaces/CarDao.java
@@ -10,8 +10,6 @@ public interface CarDao {
 
     Optional<Long> addCar(Car car) throws SQLException;
     Optional<Integer> deleteCar(long idCar) throws SQLException;
-    Optional<Integer> updateActive(long idCar, boolean status) throws SQLException;
     Optional<Car> getCarById(long idCar) throws SQLException;
     Optional<Car> getCarByDriver(long idDriver) throws SQLException;
-    Optional<List<Car>> getListCarsByStatus(boolean status) throws SQLException;
 }
