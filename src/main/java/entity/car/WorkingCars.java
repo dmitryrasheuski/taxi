1dce08e31d3521122d376217bdc07344b42d99cc
dmitryRashevsky <dmitryrasheuski@gmail.com>
2020-01-07 18:18:55 +0300

    Add the new entity to project. It is 'WorkingCar' class.

---
+++ src/main/java/entity/car/WorkingCars.java
@@ -0,0 +1,27 @@
+package entity.car;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class WorkingCars {
+    private static final List<Car> list = new ArrayList<>();
+    private static final WorkingCars INSTANCE = new WorkingCars();
+
+    private WorkingCars(){}
+    public static WorkingCars getInstance(){
+        return INSTANCE;
+    }
+
+    public boolean addCar(Car car) {
+        if (car == null) {
+            throw new IllegalArgumentException("Car is null");
+        }
+        return list.add(car);
+    }
+    public boolean removeCar(Car car) {
+        if (car == null) {
+            throw new IllegalArgumentException("Car is null");
+        }
+        return list.remove(car);
+    }
+}
