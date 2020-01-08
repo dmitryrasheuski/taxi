84b59258645f4f08816b38af63cdad29a1f62438
dmitryRashevsky <dmitryrasheuski@gmail.com>
2020-01-08 09:38:20 +0300

    Package 'web' and 'service' was removed

--- src/main/java/service/interfaces/IGetCar.java
+++
@@ -1,8 +0,0 @@
-package service.interfaces;
-
-import appException.service.AppServiceException;
-import entity.car.Car;
-
-public interface IGetCar {
-    Car getCar(long idCar) throws AppServiceException;
-}
--- src/main/java/service/interfaces/ILogin.java
+++
@@ -1,9 +0,0 @@
-package service.interfaces;
-
-import appException.service.AppServiceException;
-import entity.user.User;
-
-
-public interface ILogin {
-    User login(int phone, String password) throws AppServiceException;
-}
