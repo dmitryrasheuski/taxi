7991bbd9023b55f761872052896521dbc972d520
dmitryRashevsky <dmitryrasheuski@gmail.com>
2020-01-09 15:24:49 +0300

    An implementation of the 'service()' method is added into the 'LogoutServlet' class.

--- src/main/java/web/servlets/LogoutServlet.java
+++ src/main/java/web/servlets/LogoutServlet.java
@@ -1,16 +1,27 @@
 package web.servlets;
 
+import web.JspPages;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 @WebServlet(urlPatterns = "logout")
 public class LogoutServlet extends HttpServlet {
+    private static final JspPages nextPage = JspPages.CREATE_ORDER;
+
     @Override
     protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.service(req, resp);
+        HttpSession session = req.getSession();
+        session.invalidate();
+
+        session.setAttribute("prevPage", null);
+        session.setAttribute("nextPage", nextPage);
+
+        req.getRequestDispatcher(JspPages.PAGE_PATTERN.getPath()).forward(req, resp);
     }
 }
