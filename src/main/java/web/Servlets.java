917ba8b2c009fb5919cbeb10e3f1762e0f6e57cf
dmitryRashevsky <dmitryrasheuski@gmail.com>
2020-01-10 15:09:57 +0300

    The 'Servlets' class was refactored

--- src/main/java/web/Servlets.java
+++ src/main/java/web/Servlets.java
@@ -1,21 +1,25 @@
 package web;
 
+import lombok.AccessLevel;
+import lombok.Getter;
+
+@Getter(value = AccessLevel.PUBLIC)
 public enum Servlets {
 
-    LOGIN ("/login", JspPages.CREATE_ORDER),
-    LOGOUT ("/logout", JspPages.CREATE_ORDER),
-    REGISTER_PASSENGER ("/registryPassenger", JspPages.CREATE_ORDER),
-    REGISTRY_DRIVER ("/registryDriver", JspPages.REGISTRY_DRIVER),
+    LOGIN                       ("/login",                      JspPages.CREATE_ORDER),
+    LOGOUT                      ("/logout",                     JspPages.CREATE_ORDER),
+    REGISTER_PASSENGER          ("/registryPassenger",          JspPages.CREATE_ORDER),
+    REGISTRY_DRIVER             ("/registryDriver",             JspPages.REGISTRY_DRIVER),
 
-    CREATE_ORDER ("/createOrder", JspPages.ORDER_RESPONSE),
-    SERVE_ORDER ("/serveOrder", JspPages.CURRENT_ORDER),
-    CLOSE_ORDER ("/closeOrder", JspPages.UNPROCESSED_ORDER_LIST),
-    GET_UNPROCESSED_ORDER_LIST ("/getUnprocessedOrderList",  JspPages.UNPROCESSED_ORDER_LIST),
-    GET_TRIP_LIST ("/getTripList", JspPages.TRIP_LIST),
+    CREATE_ORDER                ("/createOrder",                JspPages.ORDER_RESPONSE),
+    SERVE_ORDER                 ("/serveOrder",                 JspPages.CURRENT_ORDER),
+    CLOSE_ORDER                 ("/closeOrder",                 JspPages.UNPROCESSED_ORDER_LIST),
+    GET_UNPROCESSED_ORDER_LIST  ("/getUnprocessedOrderList",    JspPages.UNPROCESSED_ORDER_LIST),
+    GET_TRIP_LIST               ("/getTripList",                JspPages.TRIP_LIST),
 
-    REGISTRY_CAR ("/registryCar", JspPages.REGISTRY_CAR),
-    ACTIVATE_CAR ("/activateCar", JspPages.UNPROCESSED_ORDER_LIST),
-    DEACTIVATE_CAR ("/deactivateCar", JspPages.ACTIVATING_CAR);
+    REGISTRY_CAR                ("/registryCar",                JspPages.REGISTRY_CAR),
+    ACTIVATE_CAR                ("/activateCar",                JspPages.UNPROCESSED_ORDER_LIST),
+    DEACTIVATE_CAR              ("/deactivateCar",              JspPages.ACTIVATING_CAR);
 
     private String urlPattern;
     private JspPages nextPage;
