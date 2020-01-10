dba5f60b693f3c71c07059f0d19b03fd821aab65
dmitryRashevsky <dmitryrasheuski@gmail.com>
2020-01-10 14:30:51 +0300

    The 'Servlets' class was refactored.

--- src/main/java/web/Servlets.java
+++ src/main/java/web/Servlets.java
@@ -1,24 +1,29 @@
 package web;
 
 public enum Servlets {
-    LOGIN,
-    LOGOUT,
-    REGISTER,
 
-    ADD_DRIVER,
-    ADD_CAR,
+    LOGIN ("/login", JspPages.CREATE_ORDER),
+    LOGOUT ("/logout", JspPages.CREATE_ORDER),
+    REGISTER_PASSENGER ("/registryPassenger", JspPages.CREATE_ORDER),
+    REGISTRY_DRIVER ("/registryDriver", JspPages.REGISTRY_DRIVER),
 
-    CREATE_ORDER,
-    SERVE_ORDER,
-    CLOSE_ORDER,
-    GET_ORDER_LIST,
-    GET_TRIP_LIST,
+    CREATE_ORDER ("/createOrder", JspPages.ORDER_RESPONSE),
+    SERVE_ORDER ("/serveOrder", null),
+    CLOSE_ORDER ("/closeOrder", null),
+    GET_UNPROCESSED_ORDER_LIST ("/getUnprocessedOrderList",  JspPages.UNPROCESSED_ORDER_LIST),
+    GET_TRIP_LIST ("/getTripList", JspPages.TRIP_LIST),
 
-    ACTIVATE_CAR,
-    DEACTIVATE_CAR;
+    REGISTRY_CAR ("/registryCar", JspPages.REGISTRY_CAR),
+    ACTIVATE_CAR ("/activateCar", JspPages.UNPROCESSED_ORDER_LIST),
+    DEACTIVATE_CAR ("/deactivateCar", JspPages.ACTIVATING_CAR);
 
     private String servletPath;
     private JspPages nextPage;
+
+    Servlets(String servletPath, JspPages nextPage) {
+        this.servletPath = servletPath;
+        this.nextPage = nextPage;
+    }
 
     public static JspPages getNextJspPage(String servletPath){
         return null;
