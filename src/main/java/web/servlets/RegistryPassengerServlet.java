3f0fdc31b8a75c53ec22cecf31196400e44b5ae1
dmitryRashevsky <dmitryrasheuski@gmail.com>
2020-01-10 15:42:54 +0300

    The 'RegistryServlet' class was renamed to 'RegistryPassengerServlet'

--- src/main/java/web/servlets/RegistryPassengerServlet.java
+++ src/main/java/web/servlets/RegistryPassengerServlet.java
@@ -13,22 +13,18 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
-@WebServlet(urlPatterns = "registry")
-public class RegistryServlet extends HttpServlet {
-    private static final JspPages nextPage = JspPages.CREATE_ORDER;
+@WebServlet(urlPatterns = "/registryPassenger")
+public class RegistryPassengerServlet extends HttpServlet {
     private static final IRegisterUser service = null;
 
     @Override
     protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        HttpSession session = req.getSession();
 
         User user = ServletUtils.getUserFromRequest(req);
-        user = service.register(user).orElseThrow(() -> new IllegalStateException("The user is not registered"));
-        HttpSession session = req.getSession();
+        user = service.register(user)
+                .orElseThrow(() -> new IllegalStateException("The user is not registered"));
         session.setAttribute("user", user);
 
-        session.setAttribute("prevPage", session.getAttribute("nextPage"));
-        session.setAttribute("nextPage", nextPage);
-
-        req.getRequestDispatcher(JspPages.PAGE_PATTERN.getPath()).forward(req, resp);
     }
 }
