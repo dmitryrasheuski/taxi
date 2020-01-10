02920d9efacf5f59fc05ec259f742655adab15f2
dmitryRashevsky <dmitryrasheuski@gmail.com>
2020-01-10 18:56:08 +0300

    The 'IAddPassengerInfo' class was renamed to 'IPassengerInfoProviding'

--- src/main/java/service/interfaces/user/IPassengerInfoProviding.java
+++ src/main/java/service/interfaces/user/IPassengerInfoProviding.java
@@ -4,12 +4,12 @@ import entity.user.User;
 
 import java.util.Optional;
 
-public interface IAddPassengerInfo {
+public interface IPassengerInfoProviding {
     /**
      * This method add to the database only user phone if user.status equals UserStatusType.PASSENGER
      *
      * @param passenger Type of 'User' class at which the 'status' field equals UserStatusType.PASSENGER
      * @return If the operation is successful completed then return the current user at which the 'id' field was installed, otherwise return empty Optional
      */
-    Optional<User> addInfo(User passenger);
+    Optional<User> providePassengerInfo(User passenger);
 }
