8d0381ccb35f1239555b57436f0766fd4e90c34e
dmitryRashevsky <dmitryrasheuski@gmail.com>
2020-01-13 14:29:12 +0300

    The structure of the 'OrderWaitingList' class was refactored and the new 'getOrder()' method was added

--- src/main/java/entity/order/OrderWaitingList.java
+++ src/main/java/entity/order/OrderWaitingList.java
@@ -1,23 +1,28 @@
 package entity.order;
 
+import entity.user.User;
+
+import java.util.HashMap;
 import java.util.LinkedList;
-import java.util.List;
+import java.util.Map;
+import java.util.Optional;
 
 public class OrderWaitingList {
-    private static final List<Order> list = new LinkedList<>();
+    private static final Map<User, Order> map = new HashMap<>();
     private static final OrderWaitingList INSTANCE = new OrderWaitingList();
 
-    private OrderWaitingList(){}
+    private OrderWaitingList() {}
     public static OrderWaitingList getInstance(){
         return INSTANCE;
     }
 
-    public boolean addOrder(Order order){
-        if(order == null) throw new IllegalArgumentException("Order is null");
-        return list.add(order);
+    public void addOrder(Order order) {
+        map.put(order.getPassenger(), order);
     }
-    public boolean removeOrder(Order order){
-        if(order == null) throw new IllegalArgumentException("Order is null");
-        return list.remove(order);
+    public void removeOrder(Order order) {
+        map.remove(order.getPassenger());
+    }
+    public Optional<Order> getOrderByPassenger(User passenger) {
+        return Optional.ofNullable( map.get(passenger) );
     }
 }
