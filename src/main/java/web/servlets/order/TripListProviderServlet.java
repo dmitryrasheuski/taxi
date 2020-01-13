1dc6cda077f914a3c3d2c20bd311e33448ae6fd9
dmitryRashevsky <dmitryrasheuski@gmail.com>
2020-01-13 16:29:36 +0300

    The new 'TripListPrivider' class was added into the 'src/main/java/web/servlets/order/' package.

---
+++ src/main/java/web/servlets/order/TripListProviderServlet.java
@@ -0,0 +1,26 @@
+package web.servlets.order;
+
+import entity.user.User;
+import service.interfaces.order.ITripListProviding;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+@WebServlet(urlPatterns = "/getTripList")
+public class TripListProviderServlet extends HttpServlet {
+    private static ITripListProviding listProvider = null;
+
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        HttpSession session = req.getSession();
+
+        User user = (User) session.getAttribute("user");
+        session.setAttribute("tripList", listProvider.getTripList(user));
+
+    }
+}
