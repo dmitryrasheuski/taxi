bb47f19c9781e7d80886c3b73a51208b94666353
dmitryRashevsky <dmitryrasheuski@gmail.com>
2020-01-15 13:42:14 +0300

    The new constructor was added in the 'Color' class.

--- src/main/java/entity/car/Color.java
+++ src/main/java/entity/car/Color.java
@@ -1,9 +1,11 @@
 package entity.car;
 
-import lombok.Data;
-import lombok.NonNull;
+import lombok.*;
 
 @Data
+@AllArgsConstructor
+@RequiredArgsConstructor
+@NoArgsConstructor
 public class Color {
     private Integer id;
     @NonNull
