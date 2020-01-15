b624141327ce8156fbd22ba55b9a6289f1d763d0
dmitryRashevsky <dmitryrasheuski@gmail.com>
2020-01-15 12:43:11 +0300

    The 'AbstractDao' class has been refactored

--- src/main/java/dao/impl/mysql/AbstractDao.java
+++ src/main/java/dao/impl/mysql/AbstractDao.java
@@ -1,101 +1,117 @@
 package dao.impl.mysql;
 
-import org.apache.log4j.Logger;
+import lombok.extern.log4j.Log4j;
 
 import java.sql.*;
 import java.util.List;
 import java.util.Optional;
 
+@Log4j
 abstract class AbstractDao<T> {
-    private static final Logger logger = Logger.getLogger(AbstractDao.class);
     protected MysqlDaoFactory daoFactory;
+    protected Connection con;
 
-    AbstractDao(MysqlDaoFactory factory) {
+    protected AbstractDao(MysqlDaoFactory factory) {
         this.daoFactory = factory;
-    }
 
-    Connection getConnection() throws SQLException {
-        return daoFactory.getConnection();
-
-    }
-    void startTransaction(Connection con) throws SQLException {
-        if (con != null) {
-            con.setAutoCommit(false);
+        try {
+            con = daoFactory.getConnection();
+        } catch (SQLException ex) {
+            log.error("The exception of the 'AbstractDao' class initialization had occurred at the moment of the received connection to the database", ex);
+            throw new RuntimeException("The exception of the 'AbstractDao' class initialization had occurred at the moment of the received connection to the database", ex);
         }
 
     }
-    void commit(Connection con) throws SQLException {
-        if (con != null) {
-            con.commit();
+
+    private void startTransaction() throws SQLException {
+        if (con == null) return;
+
+        con.setAutoCommit(false);
+
+    }
+    private void commit() throws SQLException {
+        if (con == null) return;
+
+        con.commit();
+        con.setAutoCommit(true);
+
+    }
+    private void rollback(Exception ex) {
+        if (con == null) return;
+
+        try {
+            con.rollback();
+        }catch (SQLException e){
+            log.error("rollback exception", e);
+            ex.addSuppressed(e);
+        }
+
+        try {
+            con.setAutoCommit(true);
+        }catch (SQLException e){
+            log.error("setAutoCommit exception", e);
+            ex.addSuppressed(e);
         }
 
     }
-    void rollback(Connection con, Exception ex) throws SQLException {
-        if (con != null) {
-            try {
-                con.rollback();
-            }catch (SQLException e){
-                logger.error("rollback exception", e);
+    private void close(AutoCloseable resource, Exception ex){
+        if (resource == null) return;
+
+        try {
+            resource.close();
+        } catch (Exception e) {
+            log.error("The exception of the '" + resource.getClass().getSimpleName() + "' class closing" , e);
+            if (ex != null) {
                 ex.addSuppressed(e);
             }
         }
+
     }
-    void close(AutoCloseable resource, Exception ex){
-        if (resource != null){
-            try {
-                resource.close();
-            } catch (Exception e) {
-                logger.error(resource.getClass().getSimpleName() + " close exception", e);
-                if (ex != null) {
-                    ex.addSuppressed(e);
-                }
-            }
+    private void preparedStatementParameterSetter(Object[] parameters, PreparedStatement ps) throws SQLException{
+        int i = 0;
+
+        for (Object param : parameters) {
+            ps.setObject(++i, param);
         }
+
     }
 
     Optional<Long> addEntity(T entity, String sqlInsert) throws SQLException {
-        Connection con = null;
         PreparedStatement ps = null;
         ResultSet rs = null;
         Exception exception = null;
+
         try {
-            con = getConnection();
-            startTransaction(con);
+            startTransaction();
 
-            ps = getPreparedStatementForAddEntity(con, ps, sqlInsert, entity);
+            ps = getPreparedStatementForAddEntity(sqlInsert, entity);
             ps.executeUpdate();
-
             rs = ps.getGeneratedKeys();
-            commit(con);
+            commit();
 
-            if(rs.next()){
-                return Optional.of(rs.getLong(1));
-            } else {
-                return Optional.empty();
-            }
+            if(!rs.next()) return Optional.empty();
+
+            return Optional.of(rs.getLong(1));
 
         } catch (Exception ex){
-            rollback(con, ex);
+            rollback(ex);
             exception = ex;
             throw ex;
 
         } finally {
             close(rs, exception);
             close(ps, exception);
-            close(con, exception);
         }
+
     }
-    Optional<List<T>> getEntityByOneValue(Object value, String sqlSelect) throws SQLException {
-        Connection con = null;
+    Optional<List<T>> getEntity(Object[] parameters, String sqlSelect) throws SQLException {
         PreparedStatement ps = null;
         ResultSet rs = null;
         Exception exception = null;
+
         try {
-            con = getConnection();
-
             ps = con.prepareStatement(sqlSelect);
-            ps.setObject(1, value);
-
+            preparedStatementParameterSetter(parameters, ps);
             rs = ps.executeQuery();
 
             return handleResultSet(rs);
@@ -107,67 +123,37 @@ abstract class AbstractDao<T> {
         } finally {
             close(rs, exception);
             close(ps, exception);
-            close(con, exception);
+
         }
     }
-    Optional<Integer> deleteById(long id, String sqlDelete) throws SQLException{
-        Connection con = null;
+    Optional<Integer> deleteOrUpdateEntity(Object[] parameters, String sqlQuery) throws SQLException{
         PreparedStatement ps = null;
         Exception exception = null;
-        try {
-            con = getConnection();
-            startTransaction(con);
-            ps = con.prepareStatement(sqlDelete);
-            ps.setLong(1, id);
-            int res = ps.executeUpdate();
-            commit(con);
 
-            if(res > 0){
-                return Optional.of(res);
-            } else {
-                return Optional.empty();
-            }
+        try {
+            startTransaction();
+
+            ps = con.prepareStatement(sqlQuery);
+            preparedStatementParameterSetter(parameters, ps);
+            int res = ps.executeUpdate();
+            commit();
+
+            if(res == 0) return Optional.empty();
+
+            return Optional.of(res);
 
         } catch (Exception ex) {
-            rollback(con, ex);
+            rollback(ex);
             exception = ex;
             throw ex;
 
         } finally {
             close(ps, exception);
-            close(con, exception);
         }
-    }
-    Optional<Integer> updateOneColumnById(long id, Object value, String sqlUpdate) throws SQLException {
-        Connection con = null;
-        PreparedStatement ps = null;
-        Exception exception = null;
-        try {
-            con = getConnection();
-            startTransaction(con);
-            ps = con.prepareStatement(sqlUpdate);
-            ps.setObject(1, value);
-            ps.setLong(2, id);
-            int res = ps.executeUpdate();
-            commit(con);
 
-            if(res > 0){
-                return Optional.of(res);
-            } else {
-                return Optional.empty();
-            }
-
-        } catch (Exception ex) {
-            rollback(con, ex);
-            exception = ex;
-            throw ex;
-
-        } finally {
-            close(ps, exception);
-            close(con, exception);
-        }
     }
 
-    abstract PreparedStatement getPreparedStatementForAddEntity(Connection con, PreparedStatement ps, String sqlInsert, T entity) throws SQLException;
+    abstract PreparedStatement getPreparedStatementForAddEntity(String sqlInsert, T entity) throws SQLException;
     abstract Optional<List<T>> handleResultSet(ResultSet rs) throws SQLException;
+
 }
