50d5f2c77c1df379d9a48765db5cc9da66e7fe1e
dmitryRashevsky <dmitryrasheuski@gmail.com>
2020-01-16 12:30:39 +0300

    fixed JavaDoc

--- src/main/java/service/interfaces/car/ICarRegistering.java
+++ src/main/java/service/interfaces/car/ICarRegistering.java
@@ -6,7 +6,8 @@ import java.util.Optional;
 
 public interface ICarRegistering {
     /**
-     * @return If the operation successful complete then return current car as which the 'id' field was installed, otherwise return empty Optional
+     * @param car this parameter must have got the not-null fields: number, color, model, driver
+     * @return an Optional discrobing the current car at which the 'id' field was installed, if the operation successful complete, otherwise return empty Optional
      */
     Optional<Car> registerCar(Car car);
 }
