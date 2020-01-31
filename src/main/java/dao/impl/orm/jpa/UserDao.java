73dd240fd9cb08fdf7435e93e1f73b6d2068200a
dmitryRashevsky <dmitryrasheuski@gmail.com>
2020-01-31 15:36:47 +0300

    The 'dao/impl/orm/jpa/UserDao' class implements the 'UserDao' interface

--- src/main/java/dao/impl/orm/jpa/UserDao.java
+++ src/main/java/dao/impl/orm/jpa/UserDao.java
@@ -3,61 +3,100 @@ package dao.impl.orm.jpa;
 import entity.user.User;
 import entity.user.UserStatus;
 
-import javax.persistence.EntityManager;
 import java.sql.SQLException;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
-public class UserDao implements dao.interfaces.UserDao {
-    private JpaDaoFactory daoFactory;
-    private EntityManager entityManager;
+public class UserDao extends AbstractDao<User> implements dao.interfaces.UserDao {
+    private static final String getUserByPhone = "SELECT u FROM User u WHERE phone = :phone";
 
     public UserDao(JpaDaoFactory daoFactory) {
-        this.daoFactory = daoFactory;
-        this.entityManager = daoFactory.getEntityManager();
+        super(daoFactory);
     }
 
     @Override
     public Optional<Long> addUser(User user) throws SQLException {
-        return Optional.empty();
-    }
 
+        Optional<User> res = addEntity(user);
+
+        return res.map(User::getId);
+    }
     @Override
     public Optional<Integer> deleteUser(long id) throws SQLException {
-        return Optional.empty();
-    }
 
+        boolean success = removeEntity(User.class, id);
+
+        return success ? Optional.of(1) : Optional.empty();
+    }
     @Override
     public Optional<Integer> updatePhone(long id, int phone) throws SQLException {
-        return Optional.empty();
-    }
 
+        boolean success = updateEntity(
+                User.class,
+                id,
+                (user) -> user.setPhone(phone)
+        );
+
+        return success ? Optional.of(1) : Optional.empty();
+    }
     @Override
     public Optional<Integer> updateName(long id, String name) throws SQLException {
-        return Optional.empty();
-    }
 
+        boolean success = updateEntity(
+                User.class,
+                id,
+                (user) -> user.setName(name)
+        );
+
+        return success ? Optional.of(1) : Optional.empty();
+    }
     @Override
     public Optional<Integer> updateSurname(long id, String surname) throws SQLException {
-        return Optional.empty();
-    }
 
+        boolean success = updateEntity(
+                User.class,
+                id,
+                (user) -> user.setSurname(surname)
+        );
+
+        return success ? Optional.of(1) : Optional.empty();
+    }
     @Override
     public Optional<Integer> updatePassword(long id, String password) throws SQLException {
-        return Optional.empty();
-    }
 
+        boolean success = updateEntity(
+                User.class,
+                id,
+                (user) -> user.setPassword(password)
+        );
+
+        return success ? Optional.of(1) : Optional.empty();
+    }
     @Override
     public Optional<Integer> updateStatus(long id, UserStatus status) throws SQLException {
-        return Optional.empty();
-    }
 
+        boolean success = updateEntity(
+                User.class,
+                id,
+                (user) -> user.setStatus(status)
+        );
+
+        return success ? Optional.of(1) : Optional.empty();
+    }
     @Override
     public Optional<User> getById(long id) throws SQLException {
-        return Optional.empty();
+        return getEntity(User.class, id);
     }
-
     @Override
     public Optional<User> getByPhone(int phone) throws SQLException {
-        return Optional.empty();
+        Map<String, Object> properties = new LinkedHashMap<>(1);
+        properties.put("phone", phone);
+
+        List<User> list = getEntities(getUserByPhone, properties);
+
+        return list.isEmpty() ? Optional.empty() : Optional.of( list.get(0) );
     }
+
 }
