2d0dffbbc378093f913134c4d5eddefb21fbc26e
dmitryRashevsky <dmitryrasheuski@gmail.com>
2020-02-04 12:25:25 +0300

    The 'deleteCar()' method has been refactored

--- src/main/java/dao/impl/orm/jpa/CarDao.java
+++ src/main/java/dao/impl/orm/jpa/CarDao.java
@@ -1,37 +1,53 @@
 package dao.impl.orm.jpa;
 
 import entity.car.Car;
+import entity.car.CarModel;
+import entity.car.Color;
 
-import javax.persistence.EntityManager;
 import java.sql.SQLException;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
-public class CarDao implements dao.interfaces.CarDao {
-    private JpaDaoFactory daoFactory;
-    private EntityManager entityManager;
+class CarDao extends AbstractDao<Car> implements dao.interfaces.CarDao {
+    private static final String getByDriverId = "SELECT c FROM Car c WHERE c.driver.getId = :driverId";
 
-    public CarDao(JpaDaoFactory daoFactory) {
-        this.daoFactory = daoFactory;
-        this.entityManager = daoFactory.getEntityManager();
+    CarDao(JpaDaoFactory daoFactory) {
+        super(daoFactory);
     }
 
     @Override
     public Optional<Long> addCar(Car car) throws SQLException {
-        return Optional.empty();
-    }
+        Color color = car.getColor();
+        color = daoFactory.getColorDao().getOrElseAddAndGet( color.getTitle() );
+        car.setColor(color);
 
+        CarModel model = car.getModel();
+        model = daoFactory.getCarModelDao().getOrElseAddAndGet(model.getTitle());
+        car.setModel(model);
+
+        return addEntity(car)
+                .map(Car::getId);
+    }
     @Override
     public Optional<Integer> deleteCar(long idCar) throws SQLException {
-        return Optional.empty();
-    }
 
+        boolean success = removeEntity(Car.class, idCar);
+
+        return success ? Optional.of(1) : Optional.empty();
+    }
     @Override
     public Optional<Car> getCarById(long idCar) throws SQLException {
-        return Optional.empty();
+        return getEntity(Car.class, idCar);
     }
-
     @Override
-    public Optional<Car> getCarByDriver(long idDriver) throws SQLException {
-        return Optional.empty();
+    public Optional<Car> getCarByDriver(long driverId) throws SQLException {
+        Map<String, Object> parameters = new LinkedHashMap<>(1);
+        parameters.put("driverId", driverId);
+
+        List<Car> list = getEntities(getByDriverId, parameters);
+
+        return list.isEmpty() ? Optional.empty() : Optional.of( list.get(0) );
     }
 }
