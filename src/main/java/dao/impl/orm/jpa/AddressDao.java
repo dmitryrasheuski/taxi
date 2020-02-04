d1b0c75389066a7afb51639e55c30a75523c5a67
dmitryRashevsky <dmitryrasheuski@gmail.com>
2020-02-04 15:01:48 +0300

    The 'orm/jpa/AddressDao' class has been implemented the 'AddressDao' class

--- src/main/java/dao/impl/orm/jpa/AddressDao.java
+++ src/main/java/dao/impl/orm/jpa/AddressDao.java
@@ -4,24 +4,46 @@ import entity.order.Address;
 
 import javax.persistence.EntityManager;
 import java.sql.SQLException;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
-public class AddressDao implements dao.interfaces.AddressDao {
-    private JpaDaoFactory daoFactory;
-    private EntityManager entityManager;
+class AddressDao extends AbstractDao<Address> implements dao.interfaces.AddressDao {
+    private static final String getByTitle = "SELECT a FROM Address a WHERE a.title = :title";
 
-    public AddressDao(JpaDaoFactory daoFactory) {
-        this.daoFactory = daoFactory;
-        this.entityManager = daoFactory.getEntityManager();
+    AddressDao(JpaDaoFactory daoFactory) {
+        super(daoFactory);
+    }
+
+    @Override
+    public Optional<Address> addAddress(Address address) throws SQLException {
+        address.setTitle(
+                address.getTitle().toUpperCase()
+        );
+
+        return addEntity(address);
+    }
+
+    @Override
+    public boolean removeAddress(Address address) throws SQLException {
+        return removeEntity(Address.class, address.getId());
     }
 
     @Override
     public Optional<Address> getAddress(long id) throws SQLException {
-        return Optional.empty();
+        return getEntity(Address.class, id);
     }
 
     @Override
     public Optional<Address> getAddress(String title) throws SQLException {
-        return Optional.empty();
+        Map<String, Object> parameters = new LinkedHashMap<>(1);
+        parameters.put("title", title.toUpperCase() );
+
+        List<Address> list = getEntities(getByTitle, parameters);
+
+        return list.isEmpty() ?
+                Optional.empty() :
+                Optional.of( list.get(0) );
     }
 }
