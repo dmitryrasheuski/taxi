e63169280ff581b79f601ca3ff118ba58265433c
dmitryRashevsky <dmitryrasheuski@gmail.com>
2020-02-04 12:40:55 +0300

    The 'AllArg' and 'NoArg' constructor has been added into the 'Car' class. The 'Entity' tags have been added into the 'Car' class.

--- src/main/java/entity/car/Car.java
+++ src/main/java/entity/car/Car.java
@@ -1,15 +1,38 @@
 package entity.car;
 
 import entity.user.User;
-import lombok.Builder;
-import lombok.Data;
+import lombok.*;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "cars")
 @Data
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
 @Builder
 public class Car {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(length = 7, nullable = false, unique = true)
     private String number;
+
+    @OneToOne
+    @JoinTable(
+            name = "car_driver",
+            joinColumns = @JoinColumn(name = "car_id", referencedColumnName = "id", nullable = false, unique = true),
+            inverseJoinColumns = @JoinColumn(name = "driver_id", referencedColumnName = "id", nullable = false, unique = true)
+    )
     private User driver;
+
+    @ManyToOne
+    @JoinColumn(name = "color_id", nullable = false)
     private Color color;
+
+    @ManyToOne
+    @JoinColumn(name = "model_id", nullable = false)
     private CarModel model;
 }
